4782c8da2ef408287f5f8ffcfbc65611be6767ee
yuito118 <72675978+yuito118@users.noreply.github.com>
2023-03-16 21:40:42 +0900

    uninstall

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // This line tells VS Code to use Tailwind's IntelliSense features
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // The accent color
+        accent: "#FB2F3E",
+        // The base color
+        base: "#fff",
+        main: {
+          dark: "#84AEE1",
+          light: "#D9F3FF",
+        },
+        // The black color
+        black: "#050505",
+      },
+      backdropBlur: {
+        glass: "blur(4px)",
+      },
+    },
   },
+  // These are the Tailwind plugins
   plugins: [],
 };
