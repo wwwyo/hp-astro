ed03b298d7920a352d6625edb5a164536e3be74e
yuito watanabe <yuito.watanabe@pulpuntex.com>
2023-05-10 23:41:28 +0900

    chore: install partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://pulpuntex.com",
-  integrations: [tailwind(), image(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), image(), sitemap(), robotsTxt(), partytown()]
 });
