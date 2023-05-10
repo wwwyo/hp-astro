a5c1b68f01fcc157d13e36ac225404db21363392
yuito watanabe <yuito.watanabe@pulpuntex.com>
2023-05-11 00:48:09 +0900

    fix: gtm not work

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pulpuntex.com",
-  integrations: [
-    tailwind(),
-    image(),
-    sitemap(),
-    robotsTxt(),
-    partytown({ config: { forward: ["dataLayer.push"] } }),
-  ],
+  integrations: [tailwind(), image(), sitemap(), robotsTxt()],
 });
