1dba67eaf8077bf116e9857592bb2f7c95898b99
yuito watanabe <yuito.watanabe@pulpuntex.com>
2023-05-10 23:09:32 +0900

    chore: install robotx.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pulpuntex.com",
-  integrations: [tailwind(), image(), sitemap()],
+  integrations: [tailwind(), image(), sitemap(), robotsTxt()],
 });
